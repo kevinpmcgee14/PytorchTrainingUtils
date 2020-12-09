a305ba13ba22d96acf919a9f0c3ff4047127e9e4
Kevin McGee <Kevin.McGee@arup.com>
2020-12-09 15:51:44 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='training_utils', 
+    version='1.0', 
+    packages=find_packages(),
+    install_requires=[
+        'pandas==1.1.4',
+        'torch==1.7.0+cu101',
+        'torchvision==0.8.1+cu101',
+        'tqdm==4.41.1',
+        'torch-lr-finder==0.2.1'
+    ],
+
+    )
