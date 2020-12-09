cc878800156578339af3abff59b4826e7f4b8cfb
Kevin McGee <Kevin.McGee@arup.com>
2020-12-09 16:02:03 -0600

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='training_utils', 
-    version='1.0', 
+    version='1.1', 
     packages=find_packages('training_utils', 'training_utils.*'),
     install_requires=[
         'pandas==1.1.4',
