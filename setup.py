aacf45016d5c655c38ded8c180f330049380405c
Kevin McGee <37749159+kevinpmcgee14@users.noreply.github.com>
2021-04-07 13:28:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.0.2', 
     packages=find_packages(),
     install_requires=[
+        'torch',
+        'torchvision',
+        'torch-lr-finder',
         'pandas==1.1.4',
-        'torch==1.7.0+cu101',
-        'torchvision==0.8.1+cu101',
-        'tqdm==4.41.1',
-        'torch-lr-finder==0.2.1'
+        'tqdm==4.41.1'
     ],
 
     )
