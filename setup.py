3f0b3ea1dbebfdef5bfa6893455f82ad764f4fd7
Kevin McGee <37749159+kevinpmcgee14@users.noreply.github.com>
2022-05-17 12:31:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='training_utils', 
-    version='0.0.2', 
+    version='0.0.3', 
     packages=find_packages(),
     install_requires=[
         'torch',
         'torchvision',
         'torch-lr-finder',
-        'pandas==1.1.4',
-        'tqdm==4.48.0'
+        'pandas>=1.1.4',
+        'tqdm>=4.48.0'
     ],
 
     )
